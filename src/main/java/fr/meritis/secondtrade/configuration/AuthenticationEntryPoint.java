9e812a14747aa12cf9796cabad680fad14eae75d
zanane-meritis <zied@MacBook-Pro-de-Zied.local>
2016-04-03 19:17:27 +0200

    commit

--- src/main/java/fr/meritis/secondtrade/configuration/AuthenticationEntryPoint.java
+++ src/main/java/fr/meritis/secondtrade/configuration/AuthenticationEntryPoint.java
@@ -1,10 +1,5 @@
 package fr.meritis.secondtrade.configuration;
 
-import javax.security.sasl.AuthenticationException;
-import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
-
-import org.springframework.security.web.authentication.Http403ForbiddenEntryPoint;
 import org.springframework.security.web.authentication.LoginUrlAuthenticationEntryPoint;
 
 public class AuthenticationEntryPoint extends LoginUrlAuthenticationEntryPoint{
--- src/main/java/fr/meritis/secondtrade/controller/LoginController.java
+++ src/main/java/fr/meritis/secondtrade/controller/LoginController.java
@@ -16,23 +16,8 @@ public class LoginController {
 		return "login";
 	}
 	
-	@RequestMapping(value = "/loginFailed", method=RequestMethod.GET)
-	public String loginFailed(ModelMap model) {
-		return "login";
-	}
-	
-	@RequestMapping(value = "/logout", method=RequestMethod.GET)
-	public String logout(ModelMap model) {
-		return "logout";
-	}
-	
 	@RequestMapping(value = "/403", method=RequestMethod.GET)
 	public String error403(ModelMap model) {
 		return "403";
-	}
-	
-	@RequestMapping(value="/login", method = RequestMethod.POST)
-	public String welcomePageConnected(ModelMap model) {	
-		return "forward:";
 	}
 }
